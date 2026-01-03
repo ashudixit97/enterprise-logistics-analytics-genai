50f61bd903df5fa922af3db67051f18f34c69693
Ashish Dixit <ashudixit97@gmail.com>
2026-01-02 21:44:29 -0800

    Finalize dbt staging model with data quality tests

--- dbt/logistics_ae/dbt/logistics_ae/tests/test_delivery_time_order.sql
+++
@@ -1,4 +0,0 @@
-select *
-from {{ ref('stg_delivery_events') }}
-where actual_ts < scheduled_ts
-  and datediff('hour', actual_ts, scheduled_ts) > 2
--- dbt/logistics_ae/dbt/logistics_ae/tests/test_detention_nonnegative.sql
+++
@@ -1,3 +0,0 @@
-select *
-from {{ ref('stg_delivery_events') }}
-where detention_minutes < 0
